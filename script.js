52506cd885a78bc939c2eb2f8c3391d74cea15c4
volhunx <158456447+volhunx@users.noreply.github.com>
2024-02-02 00:11:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let tg = window.Telegram.WebApp;
 let response = fetch('./items.json');
-let items = response.json();
+let items = response.json;
 let itemsDiv = document.getElementById("items");
 itemsDiv.innerHTML = items;
 tg.expand();
