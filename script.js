36e994e00075ac54ca03ea453e5f4976f42e9bd0
volhunx <158456447+volhunx@users.noreply.github.com>
2024-02-02 03:23:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 let tg = window.Telegram.WebApp;
-async function getItems(){
-  let items = await fetch('./items.json');
-  return items;
-}
-let itemsDiv = document.getElementById("items");
-itemsDiv.innerHTML = getItems();
-tg.expand();
 
+
+
+addItems();
+tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 tg.MainButton.setText("Order");
 tg.MainButton.show();
+
+async function addItems() {
+  let itemsDiv = document.createElement("div");
+  itemsDiv.className = "items";
+  document.body.appendChild(itemsDiv);
+  let items = await fetch('./items.json');
+  items = await items.json();
+  Object.entries(items).forEach(([key, value]) => {
+    let itemDiv = document.createElement("div");
+    itemDiv.className = "item";
+    itemDiv.innerHTML = `
+      <div class="item-emoji">${value.emoji}</div>
+      <div class="item-info">${value.name} · ${value.price}</div>
+      <button class="item-button" data-item="${key}">Add</button>
+    `;
+    itemsDiv.appendChild(itemDiv);
+  });
+}
